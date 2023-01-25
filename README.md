6315f59d0f1e362ca7ac815fa68210fc57ef26bd
Laurel Perkins <lmperkins20@gmail.com>
2023-01-24 17:03:34 -0800

    finished lab 02

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# about-me
+# Lab-02
+
+## About-Me
+
+I'm building a website all about me and my goals, using the HTML, CSS, and JavaScript I'm learning in class.
+
+### Author: Laurel Perkins
+
+### Links and Resources
+
+* I used  https://www.w3schools.com/html/html_emojis.asp to help me withusing emojis in HTML.
+
+* I used https://medium.com/markdown-monster-blog/getting-images-into-markdown-documents-and-weblog-posts-with-markdown-monster-9ec6f353d8ec to help me link the photo here in the ReadMe.
+
+### Lighthouse Accessibility Report Score
+
+![Lighthouse Score](img/Lab-02-Lighthouse.jpg)
+
+### Reflections and Comments
+
+* I'm having a lot of fun with this assignment. I think the repitition is really helping me learn code. JavaScript has been fun to learn and not that difficult so far. I'm really excited to learnwhat I can do with CSS, as I haven't used it much so far. Also, I am very, very unsure about using images in HTML...
---
+++ js/app.js
@@ -0,0 +1,98 @@
+'use strict';
+
+let username = prompt("Who's that sexy babe?");
+console.log("I'm looking respectfully");
+alert("It's " + username + ", bitch.");
+
+let hotBody = prompt("You wanna hot body?");
+
+if (hotBody.toLowerCase() === 'yes' || hotBody.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+ } else if ( hotBody.toLowerCase() === 'no' || hotBody.toLowerCase() === 'n') {
+		// console.log('Yes, you do.')
+		alert('Yes, you do.')
+  } else {
+	// console.log("I'm sorry, I don't uderstand your accent.")
+	alert("I'm sorry, I don't understand your accent.")
+	};
+
+let bugatti = prompt("You want a Bugatti?");
+
+if (bugatti.toLowerCase() === 'yes' || bugatti.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+} else if (bugatti.toLowerCase() === 'no' || bugatti.toLowerCase() === 'n') {
+		// console.log("Quit lyin'.")
+		alert("Quit lyin'.")
+ } else {
+	// console.log('Come again?')
+	alert('Come again?')
+ };
+
+let maserati = prompt("You want a Maserati?");
+
+if (maserati.toLowerCase() === 'yes' || maserati.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+ } else if (maserati.toLowerCase() === 'no' || maserati.toLowerCase() === 'n') {
+		// console.log("I don't believe you.")
+		alert("I don't believe you.")
+	 } else {
+	// console.log('Say what?')
+	alert('Say what?')
+	 };
+
+let lambo = prompt("You want a Lamborghini?");
+
+if (lambo.toLowerCase() === 'yes' || lambo.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+ } else if (lambo.toLowerCase() === 'no' || lambo.toLowerCase() === 'n') {
+		// console.log("Don't play.")
+		alert("Don't play.")
+  } else {
+	// console.log('What do you mean by that?')
+	alert('What do you mean by that?')
+	};
+
+// Questions "6" and "7" on the assignment are down below at "Live in a big mansion" and "Party in France" bc I can't cut Britney off mid chorus, srry.
+
+let sipMartini = prompt("Sip martinis?");
+
+if (sipMartini.toLowerCase() === 'yes' || sipMartini.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+} else if (sipMartini.toLowerCase() === 'no' || sipMartini.toLowerCase() === 'n') {
+		// console.log("Oh, maybe you're sober, that's okay.")
+		alert("Oh, maybe you're sober, that's okay.")
+ } else {
+	// console.log('Hm?')
+	alert('Hm?')
+ };
+
+let hotBikini = prompt("Look hot in a bikini?");
+
+if (hotBikini.toLowerCase() === 'yes' || hotBikini.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+} else if (hotBikini.toLowerCase() === 'no' || hotBikini.toLowerCase() === 'n') {
+		// console.log("Everyone looks hot in a bikini!")
+		alert("Everyone looks hot in a bikini!")
+ } else {
+	// console.log("You think you're funny, punk?")
+	alert("You think you're funny, punk?")
+ };
+
+let fancy = prompt("You wanna live fancy?");
+
+if (fancy.toLowerCase() === 'yes' || fancy.toLowerCase() === 'y') {
+	// console.log('You better work, bitch')
+	alert("You better work, bitch.")
+} else if (fancy.toLowerCase() === 'no' || fancy.toLowerCase() === 'n') {
+	// console.log("Get bougie, bitch.")
+	alert("Get bougie, bitch.")
+ } else {
+	// console.log("Quit playin' me.")
+	alert("Quit playin' me.")
+ };
